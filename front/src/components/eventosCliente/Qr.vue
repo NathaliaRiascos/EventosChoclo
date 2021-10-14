8e865f5784f7157c31c861de8e5bbf11b1c7ec62
NathaliaRiascos <riascosnathalia6@gmail.com>
2021-10-14 02:11:34 -0500

    Se agrego QR

---
+++ front/src/components/eventosCliente/Qr.vue
@@ -0,0 +1,44 @@
+<template>
+  <vue-qr :text="texto" :size="200"></vue-qr>
+</template>
+
+<script>
+import VueQr from 'vue-qr'
+
+export default {
+  data: () => ({
+    texto: 'Hola'
+  }),
+  mounted () {
+    this.getContenido()
+  },
+  methods: {
+    getContenido () {
+      const {
+        boletas,
+        show,
+        precio,
+        total,
+        correo
+      } = this.content
+
+      const text = `
+        Gracias ${correo} por tu compra
+        Deseamos que disfrutes el evento
+
+        
+        Datos de compra
+        boletas: ${boletas},
+        show: ${show},
+        Precio unitario: ${precio},
+        Pago cancelado: ${total}
+      `
+      this.texto = text
+    }
+  },
+  components: { VueQr },
+  props: {
+    content: Object
+  }
+}
+</script>
--- front/src/components/eventosCliente/formularios/Compra.vue
+++ front/src/components/eventosCliente/formularios/Compra.vue
@@ -1,18 +1,103 @@
 <template>
-  <div>
+  <div class="form">
       <p>Encuentro Virtual: Bienvenidos a Bordo - ESCANDINAVIA</p>
-      <q-input class="input" rounded outlined v-model="text" label="Rounded filled" />
+      <q-select
+      rounded
+      outlined
+      :options="boletas"
+      v-model="form.boleta"
+      lazy-rules
+      @update:model-value="getDataTicket"
+      label="Cantidad de boletas"
+      :rules="[
+        val => val !== null && val !== '' || 'Por favor selecciona la cantidad de boletas',
+      ]"
+      />
+      <q-select
+      rounded outlined
+      v-model="form.show"
+      :options="shows"
+      label="Show del evento"
+      lazy-rules
+      :rules="[
+        val => val !== null && val !== '' || 'Por favor selecciona un show',
+      ]"
+      />
+      <q-input
+      rounded outlined
+      v-model="form.precio"
+      label="Precio unitario"
+      type="number"
+      prefix="$"
+      >
+        <template v-slot:append>
+        </template>
+      </q-input>
+      <q-input
+      rounded
+      outlined
+      v-model="form.total"
+      label="Total"
+      type="number"
+      prefix="$"
+      >
+        <template v-slot:append>
+        </template>
+      </q-input>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+// import { ref } from 'vue'
+import data from 'src/components/eventosCliente/db.json'
 
 export default {
-  setup () {
-    return {
-      text: ref('')
+  data: () => ({
+    shows: null,
+    boletas: [],
+    form: {
+      boletas: '',
+      show: '',
+      precio: 0,
+      total: 0
+    }
+  }),
+  watch: {
+    'form.boleta': function (nuevoValor, valorAnterior) {
+      this.form.total = this.form.precio * nuevoValor + '.000'
+    }
+  },
+  mounted () {
+    this.getEvento()
+    this.setBoletas()
+  },
+  methods: {
+    getDataTicket () {
+      this.$emit('accion', 'Hola')
+      /// console.log(this.form)
+    },
+    getEvento () {
+      data.forEach(elem => {
+        if (elem.id === this.$route.params.id) {
+          this.shows = elem.shows.map(e => e.hour)
+          this.form.precio = elem.price
+        }
+      })
+    },
+    setBoletas () {
+      this.boletas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   }
 }
 </script>
+
+<style scoped>
+
+.form {
+  max-height: 700px;
+}
+
+.form > .input {
+  margin: 30px 0px;
+}
+</style>
